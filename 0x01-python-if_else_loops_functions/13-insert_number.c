4f9e8205002ebd4abe943bb458766344d08f3da7
Phumlile <pxnonkosi@gmail.com>
2023-05-09 13:06:52 -0700

    updated

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,49 +1,97 @@
 #include "lists.h"
-#include <stdlib.h>
+
+
 
 /**
- * insert_node - function that inserts number into a sorted singly linked list
- * @head: head of the linked list
- * @number: data of the new node
- * Return: address of new node or NULL if fails
+
+ * insert_node - inserts a new node
+
+ * at a given position.
+
+ * @head: head of a list.
+
+ * @number: index of the list where the new node is
+
+ * added.
+
+ * Return: the address of the new node, or NULL if it
+
+ * failed.
+
  */
+
 listint_t *insert_node(listint_t **head, int number)
+
 {
-	listint_t *new, *aux1 = *head, *aux2 = *head, *aux3 = *head;
-	unsigned int i = 0, idx = 0;
+
+	listint_t *new;
+
+	listint_t *h;
+
+	listint_t *h_prev;
+
+
+
+	h = *head;
 
 	new = malloc(sizeof(listint_t));
+
+
+
 	if (new == NULL)
+
 		return (NULL);
+
+
+
+	while (h != NULL)
+
+	{
+
+		if (h->n > number)
+
+			break;
+
+		h_prev = h;
+
+		h = h->next;
+
+	}
+
+
+
 	new->n = number;
-	new->next = NULL;
+
+
 
 	if (*head == NULL)
-	{	new->next = *head;
-		*head = new;
-		return (new);	}
 
-	while (aux1->next)
-	{	idx++;
-		aux3 = aux3->next;
-		if (aux1->n > number)
-		{	idx--;
-			break;	}
-		else if (aux1->next->next == NULL && aux3->n < number)
-		{	idx++;
-			break;	}
-		aux1 = aux1->next;	}
-	if (idx == 0)
-	{	new->next = *head;
+	{
+
+		new->next = NULL;
+
 		*head = new;
-		return (new);	}
-	while (aux2 != NULL && i < idx - 1)
-	{	aux2 = aux2->next;
-		i++;	}
-	if (i == idx - 1)
-	{	new->next = aux2->next;
-		aux2->next = new;
-		return (new);
+
 	}
-	return (NULL);
+
+	else
+
+	{
+
+		new->next = h;
+
+		if (h == *head)
+
+			*head = new;
+
+		else
+
+			h_prev->next = new;
+
+	}
+
+
+
+	return (new);
+
 }
